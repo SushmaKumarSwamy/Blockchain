5ecf13aa6a1f078d3a968eddcbc04d6a03bd2c59
SushmaKumarSwamy <sushmassk6@gmail.com>
2020-01-29 15:55:54 +0530

    improved hash and proof of work added

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,16 @@
-import functools
+from functools import reduce
+import hashlib as hl
+from collections import OrderedDict
+
+
+from hash_util import hash_string_256, hash_block
+
 
 genesis_block={
     'previous_hash' : '',
     'index': 0,
-    'transactions': []
+    'transactions': [],
+    'proof':100
 }
 
 MINING_REWARD = 10
@@ -13,8 +20,6 @@ owner = 'Sush'
 participants = { 'Sush' }
 
 
-def hash_block(block):
-    return '-'.join([str(block[key]) for key in block])
 
 
 def get_last_blockchain_value():
@@ -28,30 +33,48 @@ def get_balance(participant):
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [ tx['amount'] for tx in open_transaction if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda tx_sum, tx_amt : tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_sender,0)
+    amount_sent = reduce(lambda tx_sum, tx_amt : tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_sender,0)
     
     tx_receipient=  [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
-    amount_received = functools.reduce(lambda tx_sum,tx_amt:tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_receipient,0)
+    amount_received = reduce(lambda tx_sum,tx_amt:tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,tx_receipient,0)
   
     return amount_received - amount_sent
 
+
+def valid_proof(transactions,last_hash,proof):
+    guess = (str(transactions)+ str(last_hash)+str(proof)).encode()
+    guess_hash = hash_string_256(guess)
+    print(guess_hash)
+    return guess_hash[0:2]=='00'
+
+def proof_of_work():
+    last_block = blockchain[-1]
+    last_hash = hash_block(last_block)
+    proof = 0 
+    while not valid_proof(open_transaction, last_hash,proof):
+        proof +=1
+    return proof
+
 def mine_block():
-    print('before mining---------',blockchain)
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
-    reward_transaction = {
-        'sender': 'MINING',
-        'recipient': owner,
-        'amount':MINING_REWARD
-    }
+    proof = proof_of_work()
+    # reward_transaction = {
+    #     'sender': 'MINING',
+    #     'recipient': owner,
+    #     'amount':MINING_REWARD
+    # }
+    reward_transaction = OrderedDict(
+        [('sender', 'MINING'),('recipient',owner),('amount',MINING_REWARD)]
+    )
     copied_transactions = open_transaction[:]
     copied_transactions.append(reward_transaction)
     block = {'previous_hash' : hashed_block,
             'index': len(blockchain),
-            'transactions': copied_transactions
+            'transactions': copied_transactions,
+            'proof':proof
             }
     blockchain.append(block)
-    print('after mining---------',blockchain)
     return True
 
 
@@ -62,10 +85,13 @@ def add_transaction(recipient,sender = owner,amount=1.0):
         recipient: the recipient of the coin
         amount: the amount of coins sent with the transaction
     """
-    trx = { 'sender': sender,
-            'recipient': recipient,
-            'amount': amount
-        }
+    # trx = { 'sender': sender,
+    #         'recipient': recipient,
+    #         'amount': amount
+    #     }
+    trx = OrderedDict(
+        [('sender',sender),('recipient',recipient),('amount',amount)]
+    )
     if verify_transaction(trx):
         open_transaction.append(trx)
         participants.add(sender)
@@ -106,6 +132,9 @@ def block_verify():
         if index == 0 :
             continue
         if block['previous_hash'] != hash_block(blockchain[index-1]):
+            return False
+        if not valid_proof(block['transactions'][:-1],block['previous_hash'],block['proof']):
+            print('proof of wprk is invalid')
             return False
     return True
 
