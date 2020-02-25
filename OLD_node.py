066c89b418be5eb6a3332b5bceee37cdef42fbe2
SushmaKumarSwamy <sushmassk6@gmail.com>
2020-02-25 16:59:49 +0530

    flask is up running

---
+++ OLD_node.py
@@ -0,0 +1,96 @@
+from uuid import uuid4
+
+from blockchain import Blockchain
+from utility.verification import Verification
+from wallet import Wallet
+
+
+class Node:
+    def __init__(self):
+       #self.wallet.public_key = str(uuid4())
+        self.wallet = Wallet()
+        self.wallet.create_keys()
+        self.blockchain = Blockchain(self.wallet.public_key)
+
+
+    # to fetch the user values
+    def get_transaction_value(self):
+        """ takes new transaction input from the user as a float"""
+        tx_receipient = input('enter the recipient of the transaction')
+        tx_amount = float(input('enter the amount you want to send'))
+        return tx_receipient, tx_amount
+
+
+    # to take what user wants to do
+    def get_choice(self):
+        choice = input('enter your choice')
+        return choice
+
+
+    #print blocks:)
+    def print_blocks(self):
+        for block in self.blockchain.chain:
+            print('Outputting Block')
+            print(block)
+        else:
+            print('-' * 20)
+
+    def listen_to_input(self):
+        while True:
+            print('Choose:')
+            print('1: Add value to the blockchain')
+            print('2 : Mine Block')
+            print('3: print the blocks')
+            print('4: Create wallet')
+            print('5: load Wallet')
+            print('6: check transaction validity on open transaction')
+            print('7: save wallet')
+            print('q: Quit')
+            choice = self.get_choice()
+            if choice == '1':
+                tx_data = self.get_transaction_value()
+                recipient, amount = tx_data
+                signature = self.wallet.sign_transaction(self.wallet.public_key, recipient , amount)
+                if self.blockchain.add_transaction(recipient,self.wallet.public_key, signature, amount=amount):
+                    print('transaction added!!')
+                else:
+                    print('transaction failed!')
+                print(self.blockchain.get_open_transaction)
+            elif choice == '2':
+                if not self.blockchain.mine_block():
+                    print('Mining failed. Got no wallet')
+                    
+                # print(blockchain)
+            elif choice == '3':
+                self.print_blocks()
+            elif choice == '4':
+                self.wallet.create_keys()
+                self.blockchain = Blockchain(self.wallet.public_key)
+            elif choice == '5':
+                self.wallet.load_keys()
+                self.blockchain = Blockchain(self.wallet.public_key)
+            elif choice == '6':
+                if Verification.verify_transactions(self.blockchain.get_open_transaction(),self.blockchain.get_balance):
+                    print('transactions are valid')
+                else:
+                    print('transactions are not valid')
+            elif  choice == '7':
+                self.wallet.save_keys()
+            
+            elif choice == 'q':
+                break
+            else:
+                print('input was invalid, check from the list')
+
+            if not Verification.block_verify(self.blockchain.chain):
+                print('invalid chain')
+                break
+            print('Balance of {} is : {:6.2f}'.format(self.wallet.public_key,self.blockchain.get_balance()))
+        else:
+            print('user left')
+
+        print("done")
+
+# telling pyhton to execute the node.py file only when its is executed. if its imported any other file and that file is running, no need to run this
+node = Node()
+node.listen_to_input()
--- node.py
+++ node.py
@@ -1,96 +1,27 @@
-from uuid import uuid4
-
-from blockchain import Blockchain
-from utility.verification import Verification
+from flask import Flask, jsonify
+from flask_cors import CORS
 from wallet import Wallet
+from blockchain import Blockchain
+
+app = Flask(__name__)
+wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
+CORS(app)
+
+# # @app.route('/', methods=['GET'])
+# def get_ui():
+#     return 'this works'
 
 
-class Node:
-    def __init__(self):
-       #self.wallet.public_key = str(uuid4())
-        self.wallet = Wallet()
-        self.wallet.create_keys()
-        self.blockchain = Blockchain(self.wallet.public_key)
+# @app.route('/chain', methods=['GET'])
+@app.route('/', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
 
 
-    # to fetch the user values
-    def get_transaction_value(self):
-        """ takes new transaction input from the user as a float"""
-        tx_receipient = input('enter the recipient of the transaction')
-        tx_amount = float(input('enter the amount you want to send'))
-        return tx_receipient, tx_amount
-
-
-    # to take what user wants to do
-    def get_choice(self):
-        choice = input('enter your choice')
-        return choice
-
-
-    #print blocks:)
-    def print_blocks(self):
-        for block in self.blockchain.chain:
-            print('Outputting Block')
-            print(block)
-        else:
-            print('-' * 20)
-
-    def listen_to_input(self):
-        while True:
-            print('Choose:')
-            print('1: Add value to the blockchain')
-            print('2 : Mine Block')
-            print('3: print the blocks')
-            print('4: Create wallet')
-            print('5: load Wallet')
-            print('6: check transaction validity on open transaction')
-            print('7: save wallet')
-            print('q: Quit')
-            choice = self.get_choice()
-            if choice == '1':
-                tx_data = self.get_transaction_value()
-                recipient, amount = tx_data
-                signature = self.wallet.sign_transaction(self.wallet.public_key, recipient , amount)
-                if self.blockchain.add_transaction(recipient,self.wallet.public_key, signature, amount=amount):
-                    print('transaction added!!')
-                else:
-                    print('transaction failed!')
-                print(self.blockchain.get_open_transaction)
-            elif choice == '2':
-                if not self.blockchain.mine_block():
-                    print('Mining failed. Got no wallet')
-                    
-                # print(blockchain)
-            elif choice == '3':
-                self.print_blocks()
-            elif choice == '4':
-                self.wallet.create_keys()
-                self.blockchain = Blockchain(self.wallet.public_key)
-            elif choice == '5':
-                self.wallet.load_keys()
-                self.blockchain = Blockchain(self.wallet.public_key)
-            elif choice == '6':
-                if Verification.verify_transactions(self.blockchain.get_open_transaction(),self.blockchain.get_balance):
-                    print('transactions are valid')
-                else:
-                    print('transactions are not valid')
-            elif  choice == '7':
-                self.wallet.save_keys()
-            
-            elif choice == 'q':
-                break
-            else:
-                print('input was invalid, check from the list')
-
-            if not Verification.block_verify(self.blockchain.chain):
-                print('invalid chain')
-                break
-            print('Balance of {} is : {:6.2f}'.format(self.wallet.public_key,self.blockchain.get_balance()))
-        else:
-            print('user left')
-
-        print("done")
-
-# telling pyhton to execute the node.py file only when its is executed. if its imported any other file and that file is running, no need to run this
-node = Node()
-node.listen_to_input()
+if __name__ == "__main__":
+    app.run(host= '0.0.0.0', port=8080)
